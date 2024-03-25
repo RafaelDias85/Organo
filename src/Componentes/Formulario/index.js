399eb8c4ae528b6dde37353237fea0a4e9b9f856
Bowie <rafaeldiassilv@gmail.com>
2024-03-25 00:18:40 -0300

    feat: Adiciona novo recurso de formulário

--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -1,14 +1,36 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Designer',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        alert('Form foi submetido')
+    }
+    
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador. </h2>
-                <CampoTexto label="nome" placeholder="Digite seu Nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
+                <CampoTexto obrigatorio = {true} label="nome" placeholder="Digite seu Nome" />
+                <CampoTexto obrigatorio = {true} label="Cargo" placeholder="Digite seu Cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da Imagem" />
+                <ListaSuspensa obrigatorio = {true} label = "Time" itens={times} />
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
