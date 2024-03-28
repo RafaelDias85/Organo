04dc824141e63820dd55310b9d00ccb484f6afd2
Bowie <rafaeldiassilv@gmail.com>
2024-03-28 00:20:17 -0300

    feat: pequenas melhorias no Formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import Banner from './Componentes/Banner';
 import Formulario from './Componentes/Formulario';
 
 function App() {
+
+  const [colaborador, setColaborador] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+
+  }
+
+    
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />      
+      <Formulario aoColaboradorCadastrado = {colaborador => aoNovoColaboradorAdicionado(colaborador)}/>      
     </div>
   );
 }
