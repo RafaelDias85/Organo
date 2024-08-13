14e1a1af1217de98a83d1966d00438785f10ce5a
Bowie <rafaeldiassilv@gmail.com>
2024-08-13 00:03:33 -0300

    feat: inserindo componentes em Formulario - Aprendendo React

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,31 @@
+import Botao from "../Botao";
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import "./Formulario.css";
+
+const Formulario = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+
+  return (
+    <section className="formulario">
+      <form>
+        <h2>Preencha os dados para criar o card do colaborador.</h2>
+        <CampoTexto label="Nome" placeholder="Digite seu Nome" />
+        <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
+        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times} />
+        <Botao>Cria Card</Botao>
+      </form>
+    </section>
+  );
+};
+
+export default Formulario;
