d637bfca3ca168f6e2367fabd224bcd02deafcfb
Bowie <rafaeldiassilv@gmail.com>
2024-08-12 21:06:44 -0300

    Primeiro Commit - Adicionando projeto de Estudo em React.js - Organo

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,16 @@
+import "./Banner.css";
+
+function Banner() {
+  //jsx
+  return (
+    <header className="banner">
+      <img
+        className="banner"
+        src="/imagens/banner.png"
+        alt="Banner Principal da Pagina do Organo"
+      />
+    </header>
+  );
+}
+
+export default Banner;
